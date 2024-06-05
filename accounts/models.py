bf9d26f9a4621c92ff80c30af87e650bc3104487
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-06-05 09:39:42 +0200

    create signals

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from utils.generate_code import generate_code
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Profile(models.Model):
     user=models.OneToOneField(User,related_name='profile_user',on_delete=models.CASCADE)
@@ -9,4 +11,13 @@ class Profile(models.Model):
 
     def __str(self):
         return str(self.user)
+    
+
+# create signals
+@receiver(post_save,sender=User)
+def create_profile(sender,instance,created,**kwargs):
+    if created:
+        Profile.objects.create(
+            user=instance
+        )
 

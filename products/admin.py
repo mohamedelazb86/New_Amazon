fd4a808d6025009753feddfdeecb4ac041c4289a
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-06-08 18:13:51 +0200

    add admin to product models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+from .models import Product,Imgae_Product,Brand,Review
+
+class ProductImage(admin.TabularInline):
+    model=Imgae_Product
+
+class ProductAdmin(SummernoteModelAdmin):
+    list_display=['name','flag','sku','subtitle']
+    list_filter=['brand','flag']
+    search_fields=['name','subtitle','descriptions']
+    summernote_fields = ('subtitle','descriptions')
+    inlines=[ProductImage]
+    
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Brand)
+admin.site.register(Review)
+#admin.site.register(Imgae_Product)

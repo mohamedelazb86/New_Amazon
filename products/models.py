7623a0b1d339b17a90b1c75045e477f706a3b471
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-06-08 14:39:13 +0200

    add authenactions

---
+++ products/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Product(models.Model):
+    pass
+
+class Brand(models.Model):
+    pass
+class Imgae_Product(models.Model):
+    pass
+
+class Review(models.Model):
+    pass
--- project/urls.py
+++ project/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('accounts/',include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('posts/',include('posts.urls')),
     path('summernote/', include('django_summernote.urls')),

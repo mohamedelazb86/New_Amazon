a5f7d4095c88cd66e4405714cde49f74bf1931d3
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-06-09 16:55:38 +0200

    add review in products

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Product_list,Product_Detail
+from .views import Product_list,Product_Detail,add_review
 
 urlpatterns = [
     path('',Product_list.as_view()),
     path('<slug:slug>',Product_Detail.as_view()),
+    path('<slug:slug>/add_review',add_review),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import ListView,DetailView
 from .models import Product,Brand,Review,Imgae_Product
 
@@ -16,6 +16,22 @@ class Product_Detail(DetailView):
         context = super().get_context_data(**kwargs)
         context["reviews"] = Review.objects.filter(product=self.get_object())
         context["images"] = Imgae_Product.objects.filter(product=self.get_object())
+        context["products"] =Product.objects.filter(brand=self.get_object().brand)
         return context
     
+def add_review(request,slug):
+    product=Product.objects.get(slug=slug)
+   
+
+    review=request.POST['review']
+    rate=request.POST['rating']
+    Review.objects.create(
+        product=product,
+        user=request.user,
+        review=review,
+        rate=rate,
+
+    )
+    return redirect(f'/products/{slug}')
+        
 

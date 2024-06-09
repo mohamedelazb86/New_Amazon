1e65c0f57c06f9b90bf136433a4b9a7199d87780
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-06-09 14:53:27 +0200

    add review and image in product detatil

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
-from .models import Product,Brand
+from .models import Product,Brand,Review,Imgae_Product
 
 
 class Product_list(ListView):
@@ -11,4 +11,11 @@ class Product_list(ListView):
 class Product_Detail(DetailView):
     model=Product
     template_name='products/product_detail.html'
+
+    def get_context_data(self, **kwargs) :
+        context = super().get_context_data(**kwargs)
+        context["reviews"] = Review.objects.filter(product=self.get_object())
+        context["images"] = Imgae_Product.objects.filter(product=self.get_object())
+        return context
+    
 

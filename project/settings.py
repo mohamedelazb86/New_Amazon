44c49b34f51f00d27aea8d1c38dd53c42c824a3f
mohamedelazab86 <142796104+mohamedelazab86@users.noreply.github.com>
2024-06-12 17:35:43 +0200

    add treanslations

--- project/settings.py
+++ project/settings.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.locale.LocaleMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -199,3 +200,8 @@ EMAIL_HOST_PASSWORD = 'mazrfqbmquqwuapv'
 
 EMAIL_USE_SSL = False
 LOCALE_PATHS=['locale']
+
+LANGUAGES = [
+    ("ar", "Arabic"),
+    ("en", "English"),
+]
